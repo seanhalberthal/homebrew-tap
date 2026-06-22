2bf3e2c729167234a6376c61b83552e9bfcd1479
sean <91675054+seanhalberthal@users.noreply.github.com>
2026-06-22 21:16:53 +0100

    chore: rename github owner to undont

--- Formula/jiru.rb
+++ Formula/jiru.rb
@@ -1,25 +1,25 @@
 class Jiru < Formula
   desc "Terminal UI for Jira built with Bubble Tea"
-  homepage "https://github.com/seanhalberthal/jiru"
+  homepage "https://github.com/undont/jiru"
   version "0.3.18"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/seanhalberthal/jiru/releases/download/v0.3.18/jiru-darwin-arm64"
+      url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-darwin-arm64"
       sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
     else
-      url "https://github.com/seanhalberthal/jiru/releases/download/v0.3.18/jiru-darwin-amd64"
+      url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-darwin-amd64"
       sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/seanhalberthal/jiru/releases/download/v0.3.18/jiru-linux-arm64"
+      url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-linux-arm64"
       sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
     else
-      url "https://github.com/seanhalberthal/jiru/releases/download/v0.3.18/jiru-linux-amd64"
+      url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-linux-amd64"
       sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
     end
   end

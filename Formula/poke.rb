eaaba96c287b6df2553192d1fd41190bba0e7851
sean <91675054+undont@users.noreply.github.com>
2026-06-27 19:39:19 +0100

    poke: correct per-platform checksums

--- Formula/poke.rb
+++ Formula/poke.rb
@@ -15,7 +15,7 @@ class Poke < Formula
 
     on_intel do
       url "https://github.com/undont/poke/releases/download/v0.2.0/poke_darwin_amd64.tar.gz"
-      sha256 "6a39155a4e6beb02afe823f63d8fb675c2c47a9fb57be408c2d2ba5a685eb195"
+      sha256 "b29a1889f31e3156f352177d4d19e214da3072cb2cec274bedbb70a403f36ff4"
 
       def install
         bin.install "poke", "poked"
@@ -26,7 +26,7 @@ class Poke < Formula
   on_linux do
     on_arm do
       url "https://github.com/undont/poke/releases/download/v0.2.0/poke_linux_arm64.tar.gz"
-      sha256 "6a39155a4e6beb02afe823f63d8fb675c2c47a9fb57be408c2d2ba5a685eb195"
+      sha256 "0277d35d363cdceee471e921f4809922ac80c304085e6d3c2a62eed330c04373"
 
       def install
         bin.install "poke", "poked"
@@ -35,7 +35,7 @@ class Poke < Formula
 
     on_intel do
       url "https://github.com/undont/poke/releases/download/v0.2.0/poke_linux_amd64.tar.gz"
-      sha256 "6a39155a4e6beb02afe823f63d8fb675c2c47a9fb57be408c2d2ba5a685eb195"
+      sha256 "688b0cd732407a592c9349ddb608b516d2ef1547de2896eb590f63f65cf595a6"
 
       def install
         bin.install "poke", "poked"
